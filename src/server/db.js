ee562069a5913101dd8de624305c1568059ac808
Saka Adeyemi Shittu <zakatechsoftware@gmail.com>
2024-04-03 17:33:13 +0100

    mongodb set up

--- src/server/db.js
+++ src/server/db.js
@@ -1,28 +1,30 @@
-const mongoose = require('mongoose');
-const mongoURI =  "mongodb://root:<your-password>@127.0.0.1:27017";
+//const mongoose = require('mongoose');
+import mongoose from "mongoose";
+const mongoURI =
+  "mongodb+srv://zakatechsoftware:0UXADJxWxsEMSGgc@cluster0.up6m5c8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"; //0UXADJxWxsEMSGgc //"mongodb://root:<your-password>@127.0.0.1:27017";
 
 const connectToMongo = async (retryCount) => {
-    const MAX_RETRIES = 3;
-    const count = retryCount ?? 0;
-    try {
-        await mongoose.connect(mongoURI, { dbName: 'stayhealthybeta1'});
-        console.info('Connected to Mongo Successfully')
+  const MAX_RETRIES = 3;
+  const count = retryCount ?? 0;
+  try {
+    await mongoose.connect(mongoURI, { dbName: "stayhealthybeta1" });
+    console.info("Connected to Mongo Successfully");
 
-        return;
-    } catch (error) {
-        console.error(error);
+    return;
+  } catch (error) {
+    console.error(error);
 
-        const nextRetryCount = count + 1;
+    const nextRetryCount = count + 1;
 
-        if (nextRetryCount >= MAX_RETRIES) {
-            throw new Error('Unable to connect to Mongo!');
-        }
-
-        console.info(`Retrying, retry count: ${nextRetryCount}`)
-
-        return await connectToMongo(nextRetryCount);
-
+    if (nextRetryCount >= MAX_RETRIES) {
+      throw new Error("Unable to connect to Mongo!");
     }
+
+    console.info(`Retrying, retry count: ${nextRetryCount}`);
+
+    return await connectToMongo(nextRetryCount);
+  }
 };
 
-module.exports = connectToMongo;
+//module.exports = connectToMongo;
+export default connectToMongo;
